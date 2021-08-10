14d14331d0850e213147b5af50da544bb72bdaa6
worstpractice <54796463+worstpractice@users.noreply.github.com>
2021-08-10 13:24:58 +0200

    bump, fmt

--- src/utils/fromEntries.ts
+++ src/utils/fromEntries.ts
@@ -1,1 +1,3 @@
-export const fromEntries = Object.fromEntries as <K extends PropertyKey, V>(t: readonly (readonly [K, V])[]) => { readonly [key in typeof t[number][0]]: NonNullable<typeof t[number][1]> };
+export const fromEntries = Object.fromEntries as <K extends PropertyKey, V>(
+  t: readonly (readonly [K, V])[],
+) => { readonly [key in typeof t[number][0]]: NonNullable<typeof t[number][1]> };
